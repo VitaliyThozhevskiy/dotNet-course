b30fb3d34520a06612ebcc045b9c35ac79814543
Dmytro Chebotar <dmytro.chebotar@gmail.com>
2020-03-12 22:54:52 +0200

    initialized project

---
+++ PrimitiveAndConstructions/PrimitiveAndConstructions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimitiveAndConstructions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
